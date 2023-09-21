1c2483a02a69646b3a75a69863360fc6d20cc08b
sDEV-eloper <thisissurajdev@gmail.com>
2023-09-21 21:06:22 +0530

    added files with routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import Layout from "./components/Layout/Layout";
-
-
+import About from './pages/About'
+import HomePage from './pages/HomePage'
+import {Routes, Route} from 'react-router-dom'
+import Policy from "./pages/Policy";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
   return (
     <>
-   <Layout>
-    <h1>welcome to eshop</h1>
-   </Layout>
+   <Routes>
+    <Route path="/" element={<HomePage/>} />
+    <Route path="/about" element={<About/>} />
+    <Route path="/contact" element={<contact/>} />
+    <Route path="/pagenotfound" element={<PageNotFound/>} />
+    <Route path="/policy" element={<Policy/>} />
+   </Routes>
     </>
   );
 }
